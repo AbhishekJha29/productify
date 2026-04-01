506594325e85819ac1c55e0318467029b38e4d73
AbhishekJha29 <abbishekjha2286@gmail.com>
2026-04-01 18:28:26 +0530

    Final Commit

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -37,5 +37,13 @@ export const useMyProducts = () => {
 }
 
 export const useUpdateProduct = () => {
-    return useMutation({ mutationFn: updateProduct});
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: updateProduct,
+        onSuccess: (_, variables) => {
+            queryClient.invalidateQueries({queryKey: ["products"]});
+            queryClient.invalidateQueries({queryKey: ["products", variables.id]});
+            queryClient.invalidateQueries({queryKey: ["myProducts"]});
+        }
+    });
 };
