7f2fb8dddbbfdaad6823671a20566db231287eb1
AbhishekJha29 <abbishekjha2286@gmail.com>
2026-04-01 18:25:18 +0530

    Final Commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors"
 import { ENV } from "./config/env";
 import { clerkMiddleware } from '@clerk/express'
+import userRoutes from './routes/userRoutes'
+import productRoutes from './routes/productRoutes'
+import commentRoutes from './routes/commentRoutes'
 
 const app = express();
 
-app.use(cors({origin: ENV.FRONTEND_URL}));
+app.use(cors({origin: ENV.FRONTEND_URL, credentials: true}));
 app.use(clerkMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -20,5 +23,9 @@ app.get("/", (req, res) => {
         },
     });
 })
+
+app.use("/api/users", userRoutes)
+app.use("/api/products", productRoutes)
+app.use("/api/comments", commentRoutes)
 
 app.listen(3000, () => console.log("Server is up and running on PORT:", ENV.PORT))
